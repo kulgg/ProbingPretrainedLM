8762a25a04e447ab6d8e7cd34939b1bbe0ff88b7
kulgg <75735874+kulgg@users.noreply.github.com>
2022-05-21 15:33:23 +0200

    Use token classification

--- main.py
+++ main.py
@@ -1,65 +1,84 @@
-from transformers import AutoTokenizer
-from datasets import load_dataset, load_metric
-from torch.utils.data import DataLoader
-from transformers import AutoModelForSequenceClassification, get_scheduler
-from torch.optim import AdamW
-from tqdm.auto import tqdm
+from cProfile import label
+import os
+from datasets import load_dataset
 import torch
+import argparse
+from transformers import AutoModelForTokenClassification, AutoTokenizer, DataCollatorForTokenClassification, Trainer, TrainingArguments
 
-def Tokenize(data):
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    a = data["sentence"]
-    return tokenizer(a, padding="max_length", truncation=True)
+args = None
+OUTPUT_PATH = "model"
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+def tokenize_and_align_labels(examples):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+    labels = []
+    for i, label in enumerate(examples[f"pos_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:  # Set the special tokens to -100.
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                label_ids.append(label[word_idx])
+            else:
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
 
 def main():
-    dataset = load_dataset("aakanksha/udpos")
-    print(dataset["train"][0])
-    tokenized_dataset = dataset.map(Tokenize, batched=True)
-    tokenized_dataset = tokenized_dataset.remove_columns(["sentence"])
-    tokenized_dataset = tokenized_dataset.rename_column("tags", "labels")
-    tokenized_dataset.set_format("torch")
-    small_train_dataset = tokenized_dataset["train"].shuffle(seed=42).select(range(1000))
-    small_eval_dataset = tokenized_dataset["test"].shuffle(seed=42).select(range(1000))
+    print("Starting")
+    dataset = load_dataset("xtreme", "udpos.English")
+    example = dataset["train"][0]
+    print(f"Example {example}")
+    label_list = dataset["train"].features[f"pos_tags"].feature.names
+    print(f"pos labels {label_list}")
+    tokenized_dataset = dataset.map(tokenize_and_align_labels, batched=True)
+    small_train_dataset = tokenized_dataset["train"].shuffle(seed=42).select(range(500))
+    small_eval_dataset = tokenized_dataset["test"].shuffle(seed=42).select(range(500))
+    print(tokenized_dataset["train"][0])
 
-    train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
-    eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
 
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
+    model = None
+    if os.path.exists(OUTPUT_PATH):
+        model = torch.load(OUTPUT_PATH)
+    else:
+        model = AutoModelForTokenClassification.from_pretrained("distilbert-base-uncased", num_labels=len(label_list))
 
-    optimizer = AdamW(model.parameters(), lr=5e-5)
-    num_epochs = 3
-    num_training_steps = num_epochs * len(train_dataloader)
-    lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+    training_args = TrainingArguments(
+        output_dir="./results",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=args.epochs,
+        weight_decay=0.01,
+    )
 
-    device = device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=small_train_dataset,
+        eval_dataset=small_eval_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
 
-    progress_bar = tqdm(range(num_training_steps))
-    model.train()
+    if args.action == "train":
+        trainer.train()
+    elif args.action == "eval":
+        trainer.train()
 
-    for epoch in range(num_epochs):
-        for batch in train_dataloader:
-            batch = {k: v.to(device) for k, v in batch.items()}
-            outputs = model(**batch)
-            loss = outputs.loss
-            loss.backward()
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
-            progress_bar.update(1)
-    
-    metric = load_metric("accuracy")
-    model.eval()
-    for batch in eval_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
+    torch.save(model, OUTPUT_PATH)
 
-        logits = outputs.logits
-        predictions = torch.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=batch["labels"])
-    
-    metric.compute()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate or train probing model')
+    parser.add_argument('action', type=str, default="train", help='Action to perform')
+    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs')
+    args = parser.parse_args()
     main()
