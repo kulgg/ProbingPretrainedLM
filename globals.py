1c58ea55d7ca16cd1168558eb785e716bed7e2c1
kulgg <75735874+kulgg@users.noreply.github.com>
2022-07-01 17:26:44 +0200

    Change lr

--- globals.py
+++ globals.py
@@ -15,7 +15,7 @@ tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 label_vocab = collections.defaultdict(lambda: len(label_vocab))
 
 batch_size = 64
-lr = 1e-2
+lr = 1e-4
 
 # labels = {'<pad>': 9, 'O': 0, 'B-PER': 1, 'I-PER': 2, 'B-ORG': 3, 'I-ORG': 4, 'B-LOC': 5, 'I-LOC': 6, 'B-MISC': 7, 'I-MISC': 8}
 ner_label_length = 10
